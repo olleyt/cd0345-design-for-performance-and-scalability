6a2a9e848ca0dee437bd0a209f075f7090d2b394
Olley <goodnewsolley@gmail.com>
2022-10-22 15:43:07 +1100

    apply changes for task 5

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,27 @@
-# TODO: Designate a cloud provider, region, and credentials
+# Designate a cloud provider, region, and credentials
+provider "aws" {
+  profile = "udacity_project2"
+  region = "us-east-1"
+}
 
+# provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "example" {
+  count = 4
+  ami = "ami-0742b4e673072066f"
+  instance_type = "t2.micro"
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
+# provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "example" {
+  count = 2
+  ami = "ami-09d3b3274b6c5d4aa"
+  instance_type = "m4.large"
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+  tags = {
+    Name = "Udacity M4"
+  }
+}
