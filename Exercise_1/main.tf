d46e6970998066632e1cdb2c2400bfb0b78abff0
Olley <goodnewsolley@gmail.com>
2022-10-22 18:32:28 +1100

    revert changes to main.tf to create m4 EC2 instances

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,6 +1,6 @@
 # Designate a cloud provider, region, and credentials
 provider "aws" {
-  profile = "default"
+  profile = "udacity_project2"
   region = "us-east-1"
 }
 
@@ -9,8 +9,19 @@ resource "aws_instance" "Udacity_T2" {
   count = 4
   ami = "ami-0742b4e673072066f"
   instance_type = "t2.micro"
-  subnet_id = var.public_subnet_id
+
   tags = {
     Name = "Udacity T2"
+  }
+}
+
+# provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "Udacity_M4" {
+  count = 2
+  ami = "ami-09d3b3274b6c5d4aa"
+  instance_type = "m4.large"
+  subnet_id = var.public_subnet_id
+  tags = {
+    Name = "Udacity M4"
   }
 }
